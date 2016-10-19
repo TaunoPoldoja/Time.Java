d0d0bcbefc70c650fc4a7879bf64967dcb44bb4d
Tauno Põldoja <tauno.poldoja@tptlive.ee>
2016-10-19 16:22:54 +0300

    tegin ridu vähemaks

--- src/Time.java
+++ src/Time.java
@@ -4,22 +4,20 @@
 public class Time {
     public static void main(String[] args) {
         int hour = 15;
-        int minute = 18;
-        int second = 35;
+        int minute = 12;
+        int second = 10;
         System.out.print("The current time is ");
-        System.out.print(hour);
-        System.out.print(":");
-        System.out.print(minute);
-        System.out.print(":");
-        System.out.print(second);
-        System.out.println(":");
+        System.out.println(hour + ":" + minute + ":" + second );
         int midnight;
         int fullday = 86400;
         int remaining;
         midnight = (hour * 60 * 60 )+ (minute * 60) + second; // päevas on 86400 sekundit
         remaining = fullday - midnight;
-        System.out.println("seconds since midnight " + midnight);
-        System.out.print("seconds remaining in the day " + remaining);
+        System.out.println("seconds passed since midnight " + midnight);
+        System.out.println("seconds remaining  " + remaining);
+        System.out.print("Percent of the day that has passed: ");
+        System.out.println(midnight * 100 / fullday);
+
 
     }
 
